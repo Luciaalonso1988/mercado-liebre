b51e9f4dcb751e0d5902b9da51b5bec36b79029c
Lucia Alonso <luciaflorenciaalonso@gmail.com>
2022-10-15 20:17:31 -0300

    Estructura - paso 1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const port = 3000;
+
+const publicPath = path.resolve(__dirname, './public')
+app.use(express.static(publicPath));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+})
+app.listen(port, () => {
+    console.log(`Servidor correndo en puerto ${port}`);
+})
